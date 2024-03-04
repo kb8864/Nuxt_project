1bf717435e768b41fec1604868704746f69a1a35
kb8864 <yt7s858864@gmail.com>
2024-03-04 21:09:13 +0900

    非同期通信：1人目のデータを表示

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p>{{ users[0].id }}, {{ users[0].name }}</p>
+  </div>
+</template>
+
+<script setup >
+  const {data: users} = await useFetch('https://jsonplaceholder.typicode.com/users')
+</script>
