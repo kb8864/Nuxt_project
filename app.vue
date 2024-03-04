0f8ed470a5488bd1fe1726a7ea8474174de47e2f
kb8864 <yt7s858864@gmail.com>
2024-03-04 21:51:58 +0900

    非同期通信：1人のデータを表示

--- app.vue
+++ app.vue
@@ -1,10 +1,8 @@
-
-import { NuxtLayout } from '#build/components';
 <template>
   <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
+    <p>{{ users[0].id }}, {{ users[0].name }} </p>
   </div>
 </template>
-
+<script setup>
+  const { data: users } = await useFetch('https://jsonplaceholder.typicode.com/users')
+</script>
