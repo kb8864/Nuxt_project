e194bccd73a9886305da34d6d4ff01042c80d6c5
kb8864 <yt7s858864@gmail.com>
2024-03-17 17:48:46 +0900

    スタイルの追加

--- nuxt3_projects/nuxt3-useState/app.vue
+++ nuxt3_projects/nuxt3-useState/app.vue
@@ -1,20 +1,54 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <button @click="$event =>titleState.changeTitle('useStateを更新した')">useStateを更新</button>
-    <!-- <button @click="updateInit">初期値を更新</button> -->
+    <!-- 修正前<button @click="$event =>titleState.changeTitle('useStateを更新した')">useStateを更新</button> -->
+    <!-- ボタンクリックでtitleの値を更新 -->
+    <button @click="changeTitle('useStateを更新した')">useStateを更新</button>
   </div>
 </template>
 
 <script setup>
 
+// 'composables' ディレクトリ内の 'index.ts' をインポートする。これは app.vue ファイルから相対的に composables ディレクトリ内の index.ts ファイルを見つけるため。
+import { useTitle } from './composables/index';
+
 // useStateのキーの値と初期値を設定->削除
 // index.tsで記述した自動的にインポートされるcomposables/ディレクトリを呼び出す処理を記述
-  const titleState = useTitle()
-  const {title} = titleState
+  // const titleState = useTitle()
+  // const {title} = titleState
+  // useTitleからtitleとchangeTitleを展開
+const { title, changeTitle } = useTitle();
 
-  // function updateInit (){
-  //   title.value = "更新後の値"
-  // }
+
 
 </script>
+
+<style scoped>
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+  text-align: center;
+  
+}
+
+h1 {
+  color: #e762a2;
+  margin-bottom: 20px;
+}
+
+button {
+  background-color: #ff3700;
+  color: #655d5dba;
+  border: none;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 5px;
+  transition: background-color 0.3s;
+}
+
+button:hover {
+  background-color: #0ee67d;
+}
+</style>
--- nuxt3_projects/nuxt3-useState/composables/index.ts
+++ nuxt3_projects/nuxt3-useState/composables/index.ts
@@ -1,17 +1,32 @@
 //  以下の方法でuseTitleを自動的に呼び出せる。つまりapp.vueに
 // 書いたキーと初期値設定はけす
-import { ref } from "vue";
+
+// カプセル化の強化。Vueのrefとreadonlyをインポート
+import { ref, readonly } from 'vue';
 
 export const useTitle = () => {
-      // 状態管理で保持している値を受け取った引数に変更する処理を記述
-  const title = useState<string>('title', ()=> "useStateを使用した初期値")
-  const changeTitle = (title: Ref<string>) => (value: string) => {
-    title.value = value
-  }
+    //ref の使用: Vueのリアクティブシステム内でプリミティブ値をリアクティブに扱うため
+      // これにより、アプリケーションのどの部分からでもこの状態を参照し、更新することが可能
+      // 修正前const title = useState<string>('title', ()=> "useStateを使用した初期値")
+    // refを使用してリアクティブなデータを作成
+    const title = ref('useStateを使用した初期値');
+
+
+
+  // title 状態の値を更新するために使用
+  // 引数として受け取った value を title 状態に代入して更新
+  const changeTitle = (newValue: string) => {
+    console.log('changeTitle called with:', newValue);
+    title.value = newValue;
+  };
+
   //useTitleで返却する値を記述
+  // titleを読み取り専用にして外部からの直接変更を防ぎ、
+  // changeTitle関数を通じてのみ更新を許可
   return{ 
     title: readonly(title),
-    changeTitle: changeTitle(title)
+    // 修正前changeTitle: changeTitle(title)
+    changeTitle
   }
 }
 
